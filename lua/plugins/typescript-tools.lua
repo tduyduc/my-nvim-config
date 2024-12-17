87811ed171f4007df0bf35faf999d5a2af1c6e01
T.D. Stoneheart <tduyduc3@gmail.com>
2024-12-17 19:43:56 +0700

    Resurrect "Organize Imports" hotkey

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -3,5 +3,13 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     opts = {},
+    keys = {
+      {
+        "<leader>co",
+        ":TSToolsOrganizeImports<CR>",
+        mode = { "n", "v" },
+        desc = "Organize Imports",
+      },
+    },
   },
 }
