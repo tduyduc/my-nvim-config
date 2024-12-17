50063764c59bc988628a362442a10a89aae00d19
T.D. Stoneheart <tduyduc3@gmail.com>
2024-12-17 18:55:51 +0700

    Remove mini-indentscope (replaced by snacks.indent)

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- import/override with your plugins
     { import = "plugins" },
