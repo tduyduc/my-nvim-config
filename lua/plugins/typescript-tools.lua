9d520ba8ddc0772f3dbd11507b7e70233fb902ce
T.D. Stoneheart <tduyduc3@gmail.com>
2026-02-27 08:51:50 +0700

    Configure typescript-tools even more

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -2,10 +2,17 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        complete_function_calls = false,
-      },
-    },
+    config = function()
+      require("typescript-tools").setup({
+        settings = {
+          complete_function_calls = false,
+          expose_as_code_action = "all",
+          jsx_close_tag = {
+            enable = false,
+            filetypes = { "javascriptreact", "typescript", "typescriptreact" },
+          },
+        },
+      })
+    end,
   },
 }
