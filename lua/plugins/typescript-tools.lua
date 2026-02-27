95eca5a826d3322ea139a004065a3c6a324dcbce
T.D. Stoneheart <tduyduc3@gmail.com>
2026-02-27 08:46:52 +0700

    Disable function call completion in TypeScript

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -2,6 +2,10 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {},
+    opts = {
+      settings = {
+        complete_function_calls = false,
+      },
+    },
   },
 }
